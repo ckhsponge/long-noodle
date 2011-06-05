bb798582609defe1c650f22111832007b0c08969
Chris Hobbs <ckh@CKH-MBP.local>
2011-06-04 18:43:36 -0700

    interface

--- web.js
+++ web.js
@@ -5,5 +5,5 @@ var port = process.env.PORT || 8000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(port, "127.0.0.1");
+}).listen(port, "0.0.0.0");
 console.log("Listening on " + port);
